6fdbcee03101f00ed846f9e979a54d71fc2cb924
achast <colehast17@gmail.com>
2023-03-12 21:01:05 +0800

    Add getPlayerChoice validation to either rock, paper or scissors.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,31 @@
 function getComputerChoice() {
     randomWholeNumber = Math.floor(Math.random() * 3);
     if (randomWholeNumber === 0) {
-        choice = 'Rock';
+        choice = 'rock';
     } else if (randomWholeNumber === 1) {
-        choice = 'Paper';
+        choice = 'paper';
     } else {
-        choice = 'Scissors'
+        choice = 'scissors'
     }
     return choice
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice() {
+    playerSelection = prompt("Hi! Let's play Rock, Paper, Scissors. It's your move!").toLowerCase()
+    
+    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+       playerSelection = prompt(playerSelection + " is not valid. Please pick: rock, paper, or scissors.");
+    }
+    
+    return playerSelection 
+}
+
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = getPlayerChoice()
+    computerSelection = getComputerChoice()
+
+    console.log(playerSelection, computerSelection)
+}
+
+playRound()
