e72984a3b2f0f9addb202f36dd06c7ab660f0f34
achast <colehast17@gmail.com>
2023-03-12 21:39:01 +0800

    Finish basic round function. Add win conditions to single round.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,21 +11,58 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    playerSelection = prompt("Hi! Let's play Rock, Paper, Scissors. It's your move!").toLowerCase()
-    
-    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-       playerSelection = prompt(playerSelection + " is not valid. Please pick: rock, paper, or scissors.");
+    playerSelection = prompt("Hi! Let's play Rock, Paper, Scissors. It's your move!")
+
+    if (playerSelection === null) {
+        return;
     }
     
-    return playerSelection 
+    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+       playerSelection = prompt("Sorry, " + playerSelection + " is not valid. Please pick: rock, paper, or scissors.");
+    }
+
+    return playerSelection
 }
 
 
 function playRound (playerSelection, computerSelection) {
+    userScore = 0; 
+    computerScore = 0;
+
     playerSelection = getPlayerChoice()
     computerSelection = getComputerChoice()
 
-    console.log(playerSelection, computerSelection)
+    if (playerSelection === computerSelection) {
+        alert("It's a tie, play again!")
+        return playRound()
+    }
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            console.log("Player wins!")
+            return userScore++;
+        } else {
+            console.log("Computer wins!")
+            return computerScore++;
+        }
+    }
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            console.log("Player wins!")
+            return userScore++;
+        } else {
+            console.log("Computer wins!")
+            return computerScore++;
+        }
+    }
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            console.log("Player wins!")
+            return userScore++;
+        } else {
+            console.log("Computer wins!")
+            return computerScore++;
+        }
+    }
 }
 
 playRound()
